de6886caa065333a7efa15fcd71ce9f19bfeac41
rani10245kumari <ranik10245@gmail.com>
2023-11-10 22:07:58 +0530

    second

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const jsonData = require('./jsonPackage')
 const server = http.createServer((request, response) => {
     if (request.url == '/') {
         response.write("<h1>this is my HHTP Home Service Page...</h1>")
+        response.write("<h1>What is node Js?</h1><h2>Node js is a javascript runtime environment built in chrome v8 javascript engine</h2><ol>Advantage of node js-<li>Open source</li><li>Cross Platform</li><li>Real time application used</li></ol>")
         response.end();
     }
     else if (request.url == '/products') {
